eb891fee2439967fbe667863c33d09d7a6778751
unknown <tcmobile0212602@gmail.com>
2024-09-21 14:17:38 +0700

    Learning numpy

---
+++ test.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    scores = [int(score) for score in input('Enter scores separated by spaces: ').split()]
+    sum_score = sum(scores)
+max_score = max(sum_score)
+
+
+for n in i:
+    print(f'{max_score}')
